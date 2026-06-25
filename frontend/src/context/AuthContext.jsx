f50fcf39cc94daa765719130668779d27d5d9ebd
cat <klit2406@gmail.com>
2026-06-25 08:22:25 +0700

    update context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,10 +4,13 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(
+    localStorage.getItem("token")
+  );
 
   return (
     <AuthContext.Provider
-      value={{ user, setUser }}
+      value={{ user, setUser, token, setToken }}
     >
       {children}
     </AuthContext.Provider>
